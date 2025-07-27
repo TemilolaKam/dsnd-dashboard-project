2e5de9a45f0d509b692cb3a98c72ae565248a9f1
Temilola Gbadamosi-Adeniyi <tgbadam@ncsu.edu>
2025-07-26 20:06:30 -0400

    update

--- python-package/employee_events/query_base.py
+++ python-package/employee_events/query_base.py
@@ -1,27 +1,30 @@
 # Import any dependencies needed to execute sql queries
-# YOUR CODE HERE
+import pandas as pd
+from typing import List
+from .sql_execution import QueryMixin
 
 # Define a class called QueryBase
 # Use inheritance to add methods
 # for querying the employee_events database.
-# YOUR CODE HERE
+class QueryBase(QueryMixin):
 
     # Create a class attribute called `name`
     # set the attribute to an empty string
-    # YOUR CODE HERE
+    name: str = ""
 
     # Define a `names` method that receives
     # no passed arguments
     # YOUR CODE HERE
-        
+    def names(cls) -> List[tuple]:
         # Return an empty list
-        # YOUR CODE HERE
+        return []
 
 
     # Define an `event_counts` method
     # that receives an `id` argument
     # This method should return a pandas dataframe
-    # YOUR CODE HERE
+    @classmethod
+    def event_counts(cls, _id: int) -> pd.DataFrame:
 
         # QUERY 1
         # Write an SQL query that groups by `event_date`
@@ -31,20 +34,34 @@
         # Use f-string formatting to set the name
         # of id columns used for joining
         # order by the event_date column
-        # YOUR CODE HERE
+        sql = f"""
+            SELECT   event_date,
+                     SUM(positive_events)  AS positive_events,
+                     SUM(negative_events)  AS negative_events
+            FROM     employee_events
+            JOIN     {cls.name}
+                     USING({cls.name}_id)
+            WHERE    {cls.name}.{cls.name}_id = {_id}
+            GROUP BY event_date
+            ORDER BY event_date
+        """
+        return cls.pandas_query(sql)
             
     
 
     # Define a `notes` method that receives an id argument
     # This function should return a pandas dataframe
-    # YOUR CODE HERE
+    @classmethod
+    def notes(cls, _id: int) -> pd.DataFrame:
 
-        # QUERY 2
-        # Write an SQL query that returns `note_date`, and `note`
-        # from the `notes` table
-        # Set the joined table names and id columns
-        # with f-string formatting
-        # so the query returns the notes
-        # for the table name in the `name` class attribute
-        # YOUR CODE HERE
+        sql = f"""
+            SELECT   note_date,
+                     note
+            FROM     notes
+            JOIN     {cls.name}
+                     USING({cls.name}_id)
+            WHERE    {cls.name}.{cls.name}_id = {_id}
+            ORDER BY note_date
+        """
+        return cls.pandas_query(sql)
 
